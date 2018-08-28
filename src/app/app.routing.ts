191911616aa8e358706511b05d1411abda534cf0
Junior Abreu <juniorabreu201@gmail.com>
2018-08-27 20:38:38 -0400

    natvar

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,10 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
-// import { AuthGuard } from './guards/auth.guard';
+// import { AuthGuardService } from './services/auth-guard.service';
  
 const appRoutes: Routes = [
-    // { path: '', component: HomeComponent, canActivate: },
+    // { path: '', component: HomeComponent, canActivate: [AuthGuardService]},
     { path: '', component: HomeComponent},
     { path: 'login', component: LoginComponent },
  
