9bf8ded8be1f712a2324cf375cd7e768cc1e5834
Junior Abreu <juniorabreu201@gmail.com>
2018-09-03 12:07:11 -0400

    agregando form de registrar estudiante

--- src/app/components/reg-estudiante/reg-estudiante.component.ts
+++ src/app/components/reg-estudiante/reg-estudiante.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {FuncionesService} from '../../services/funciones.service';
 import {PeticionesService} from '../../services/peticiones.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-reg-estudiante',
@@ -9,9 +12,30 @@ import {PeticionesService} from '../../services/peticiones.service';
 })
 export class RegEstudianteComponent implements OnInit {
 	val5: string;
-  constructor(private _funtions: FuncionesService, private _peticiones :PeticionesService) { }
+	formPerson: FormGroup;
+  constructor(private fb: FormBuilder,private _funtions: FuncionesService, private _peticiones :PeticionesService) { }
 
   ngOnInit() {
+  	this.createForm();
   }
 
+  createForm(){
+  	this.formPerson = this.fb.group({
+      nombres: ['', Validators.required],
+      apellidos: ['', Validators.required],
+      direccion: '',
+      cedula:'',
+      email:'',
+      carrera:'',
+      telefono:'',
+      usuario:'',
+      matricula:'',
+      carnet:'',
+      clave:'',
+      claveConfirm:''
+    });
+  }
+  onSubmit(){
+  	console.log("onSubmit ",this.formPerson.value)
+  }
 }
