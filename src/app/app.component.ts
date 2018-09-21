c0fa7f99a8946dd1a11b6019b9d98191d71770c2
Junior Abreu <juniorabreu201@gmail.com>
2018-09-21 12:47:28 -0400

    correcion de error al iniciar login por primera vez

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,OnDestroy } from '@angular/core';
+import {Observable,Subject} from "rxjs";
 import {FuncionesService} from './services/funciones.service';
 import {PeticionesService} from './services/peticiones.service';
 @Component({
@@ -6,20 +7,28 @@ import {PeticionesService} from './services/peticiones.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
 
   title = 'app';
+  private onDestroy$ = new Subject<void>();
   TotalDeConferencias;
 	constructor(private _funtions: FuncionesService,private _peticiones:PeticionesService) {
 		this.actividadesDehoy();
+    let t= Observable.interval(1000*60).takeUntil(this.onDestroy$);
+    t.subscribe(i => this.actividadesDehoy(false));
 	}
 
+  ngOnDestroy(): void {
+    this.onDestroy$.next();
+  }
 
   isAdmin(){
     return this._funtions.getLoggedUser().tipo == 'A'
   }
-  actividadesDehoy(){
-  	this._funtions.blockUIO().start()
+
+  actividadesDehoy(lMos=true){
+    if (lMos)
+  	  this._funtions.blockUIO().start()
       this._peticiones.GetConferenciasDeHoy().subscribe(
         response => {
            this._funtions.blockUIO().stop()
