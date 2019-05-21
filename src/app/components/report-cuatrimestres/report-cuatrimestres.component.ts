d48599d9410a63b142a4ef79ef9cb00765b33bcc
Pablo384 <pabloreinoso0@gmail.com>
2019-05-21 12:04:46 -0400

    reporte cuatrimestre

--- src/app/components/report-cuatrimestres/report-cuatrimestres.component.ts
+++ src/app/components/report-cuatrimestres/report-cuatrimestres.component.ts
@@ -14,6 +14,7 @@ import * as printJS from 'print-js';
 export class ReportCuatrimestresComponent implements OnInit {
   ListDeActividades = [];
   listCuatrimestres = [];
+  cuatrimestreSeleccionado;
   constructor(private _funtions: FuncionesService, private _peticiones: PeticionesService) { }
 
   ngOnInit() {
